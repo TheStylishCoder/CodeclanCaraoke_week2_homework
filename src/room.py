076971d800cb057c1d43faa108027bd2a33077df
Anna Cunningham <ac232@hotmail.co.uk>
2021-01-15 18:14:14 +0000

    first tests for extension 1

--- src/room.py
+++ src/room.py
@@ -1,9 +1,11 @@
 class Room:
 
-    def __init__(self, name):
+    def __init__(self, name, capacity):
         self.name = name
+        self.capacity = capacity
         self.playlist = []
         self.guestlist = []
+        
 
 
     def check_in_guest_to_room(self, guest):
@@ -14,3 +16,7 @@ class Room:
 
     def add_song_to_playlist(self, song):
         self.playlist.append(song)
+
+    def room_has_met_capacity(self):
+        return len(self.guestlist) >= self.capacity
+           
--- tests/room_test.py
+++ tests/room_test.py
@@ -6,12 +6,15 @@ from src.song import Song
 class TestRoom(unittest.TestCase):
 
     def setUp(self):
-        self.room = Room("Booth 1")
+        self.room = Room("Booth 1", 4)
         self.guest = Guest("Pam Halpert")
         self.song = Song("Love Shack", "The B-52's")
 
     def test_room_has_name(self):
         self.assertEqual("Booth 1", self.room.name)
+
+    def test_room_has_capacity(self):
+        self.assertEqual(4, self.room.capacity)
 
     def test_room_playlist_starts_empty(self):
         self.assertEqual(0, len(self.room.playlist))
@@ -32,3 +35,14 @@ class TestRoom(unittest.TestCase):
         self.room.add_song_to_playlist(self.song)
         self.assertEqual(1, len(self.room.playlist))
 
+    def test_room_has_met_capacity_returns_true(self):
+        self.room.check_in_guest_to_room(self.guest)
+        self.room.check_in_guest_to_room(self.guest)
+        self.room.check_in_guest_to_room(self.guest)
+        self.room.check_in_guest_to_room(self.guest)
+        self.assertEqual(True, self.room.room_has_met_capacity())
+
+    def test_room_has_met_capacity_returns_false(self):
+        self.room.check_in_guest_to_room(self.guest)
+        self.room.check_in_guest_to_room(self.guest)
+        self.assertEqual(False, self.room.room_has_met_capacity())
