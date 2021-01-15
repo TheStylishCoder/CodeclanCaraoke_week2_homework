56f98137126f91be09e360299b96113bf87be70e
Anna Cunningham <ac232@hotmail.co.uk>
2021-01-15 15:21:55 +0000

    Further room_tests passing

--- src/room.py
+++ src/room.py
@@ -3,3 +3,8 @@ class Room:
     def __init__(self, name):
         self.name = name
         self.playlist = []
+        self.guestlist = []
+
+
+    def check_in_guest_to_room(self, guest):
+        self.guestlist.append(guest)
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,10 +1,12 @@
 import unittest
 from src.room import Room 
+from src.guest import Guest
 
 class TestRoom(unittest.TestCase):
 
     def setUp(self):
         self.room = Room("Booth 1")
+        self.guest = Guest("Pam Halpert")
 
     def test_room_has_name(self):
         self.assertEqual("Booth 1", self.room.name)
@@ -12,4 +14,9 @@ class TestRoom(unittest.TestCase):
     def test_room_playlist_starts_empty(self):
         self.assertEqual(0, len(self.room.playlist))
 
-        
+    def test_room_guestlist_starts_at_0(self):
+        self.assertEqual(0, len(self.room.guestlist))
+
+    def test_check_in_guest_to_room(self):
+        self.room.check_in_guest_to_room(self.guest)
+        self.assertEqual(1, len(self.room.guestlist))
