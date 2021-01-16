c96ff7be154bcbb4f599d6511f6f8fe4aca9ce2b
Anna Cunningham <ac232@hotmail.co.uk>
2021-01-16 14:12:07 +0000

    Added further instances of guest in room_test

--- tests/room_test.py
+++ tests/room_test.py
@@ -16,12 +16,18 @@ class TestRoom(unittest.TestCase):
         self.song_8 = Song("Disco 2000", "Pulp")
         self.song_9 = Song("Don't Go Breaking My Heart", "Elton John & Kiki Dee")
         self.song_10 = Song("You Spin Me Round", "Dead Or Alive")
+        self.song_11 = Song("Love Shack", "The B-52's")
 
         playlist = [self.song_1, self.song_2, self.song_3, self.song_4, self.song_5, self.song_6, self.song_7, self.song_8, self.song_9, self.song_10]
        
         self.room = Room("Booth 1", 4, playlist) 
-        self.guest = Guest("Pam Halpert")
-        self.song = Song("Love Shack", "The B-52's")
+
+        self.guest_1 = Guest("Pam Halpert")
+        self.guest_2 = Guest("Jim Halpert")
+        self.guest_3 = Guest("Michael Scott")
+        self.guest_4 = Guest("Dwight Schrute")
+        self.guest_5 = Guest("Angela Martin")
+        
 
     def test_room_has_name(self):
         self.assertEqual("Booth 1", self.room.name)
@@ -29,9 +35,7 @@ class TestRoom(unittest.TestCase):
     def test_room_has_capacity(self):
         self.assertEqual(4, self.room.capacity)
 
-    # def test_room_playlist_starts_empty(self):
-    #     self.assertEqual(0, len(self.room.playlist))
-
+  
     def test_room_has_playlist(self):
         self.assertEqual(10, len(self.room.playlist))
 
@@ -39,26 +43,26 @@ class TestRoom(unittest.TestCase):
         self.assertEqual(0, len(self.room.guestlist))
 
     def test_check_in_guest_to_room(self):
-        self.room.check_in_guest_to_room(self.guest)
+        self.room.check_in_guest_to_room(self.guest_1)
         self.assertEqual(1, len(self.room.guestlist))
 
     def test_check_out_guest_from_room(self):
-        self.room.check_in_guest_to_room(self.guest)
-        self.room.check_out_guest_from_room(self.guest)
+        self.room.check_in_guest_to_room(self.guest_1)
+        self.room.check_out_guest_from_room(self.guest_1)
         self.assertEqual(0, len(self.room.guestlist))
 
     def test_can_add_songs_to_room_playlist(self):
-        self.room.add_song_to_playlist(self.song)
+        self.room.add_song_to_playlist(self.song_11)
         self.assertEqual(11, len(self.room.playlist))
 
     def test_room_has_met_capacity_returns_true(self):
-        self.room.check_in_guest_to_room(self.guest)
-        self.room.check_in_guest_to_room(self.guest)
-        self.room.check_in_guest_to_room(self.guest)
-        self.room.check_in_guest_to_room(self.guest)
+        self.room.check_in_guest_to_room(self.guest_1)
+        self.room.check_in_guest_to_room(self.guest_2)
+        self.room.check_in_guest_to_room(self.guest_3)
+        self.room.check_in_guest_to_room(self.guest_4)
         self.assertEqual(True, self.room.room_has_met_capacity())
 
     def test_room_has_met_capacity_returns_false(self):
-        self.room.check_in_guest_to_room(self.guest)
-        self.room.check_in_guest_to_room(self.guest)
+        self.room.check_in_guest_to_room(self.guest_1)
+        self.room.check_in_guest_to_room(self.guest_2)
         self.assertEqual(False, self.room.room_has_met_capacity())
